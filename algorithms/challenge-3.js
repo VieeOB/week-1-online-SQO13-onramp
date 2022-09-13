cbcc3804a749f625dac7965581e5906aef661cce
Bodunde Victor .O <bodundevictor91@gmail.com>
2022-09-13 02:12:30 +0000

    Update 3 files

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,4 +1,7 @@
 function sumMix(arr) {
+    parseInt([6, 8, '3', 5, '9', 8, ]) to return 27
+    parseInt(['5', 3, 4, '0', 3,  '2',]) to return 10
+    parseInt(['9', 4, 6, '3', 5,]) to return 15
    
  }
 
--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,4 +1,7 @@
 function calculateGrade(marks) {
- 
+    calculateGrade([80, 80, 70]) to return 'C'
+    calculateGrade([19, 5, 42, 2, 77]) to return 'F'
+    calculateGrade([59, 80, 40,2,77]) to return 'E'
+    calculateGrade([89, 50,40,90]) to return 'C'
  }
   module.exports =calculateGrade
